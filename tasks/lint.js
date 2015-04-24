1c1240cc53d157cba9e06c9223e25bcc8bf2e911
Ravi Kotecha <kotecha.ravi@gmail.com>
2015-04-24 16:07:53 +0100

    linting for CI

--- tasks/lint.js
+++ tasks/lint.js
@@ -5,13 +5,20 @@ var paths = require('./_paths');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var filter = require('gulp-filter');
+var gulpif = require('gulp-if');
 
-gulp.task('lint', function() {
-  var withoutTemplates = filter(['**/*.js', '!**/*templates.js']);
+var lintFn = function(failFast) {
+  return function() {
+    var withoutTemplates = filter(['**/*.js', '!**/*templates.js']);
 
-  gulp
-    .src(paths.scripts)
-    .pipe(withoutTemplates)
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish));
-});
+    gulp
+      .src(paths.scripts)
+      .pipe(withoutTemplates)
+      .pipe(jshint())
+      .pipe(jshint.reporter(stylish))
+      .pipe(gulpif(failFast, jshint.reporter('fail')));
+  };
+};
+
+gulp.task('lint', lintFn(false));
+gulp.task('lint-ci', lintFn(true));
